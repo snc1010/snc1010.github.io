5fa901d919d0434cc777e6dc412a55dc9ca6646b
Sarah Cooke <sarahc1010@gmail.com>
2018-07-02 23:14:50 -0400

    Cleaned up; added comments

--- js/app.js
+++ js/app.js
@@ -1,26 +1,16 @@
-//$(document).ready(function(){
-//console.log($("body").length)
-//$("div").css('cursor','url(../img/hotdogwithmustardandketchup_vw0_icon.icog), default');
-//});
-
-//example jQuery animation for header
-//$('#flashMessage').hide().slideDown(1000).delay(3000).slideUp();
-
-
+//Open secure links in new tab
 const $secureLinks = $('a[href^="https://"]');
 $secureLinks.attr('target', '_blank');
 
+//Open non-secure links in new tab
 const $nonsecureLinks = $('a[href^="http://"]');
 $nonsecureLinks.attr('target', '_blank');
 
 //slide down "I ❤️ hot dogs"
-//$('#intro').hide().slideDown(3500);
+$('#intro').hide().slideDown(3500);
 
 
-
-
-  //const heartArray = [0x1F9E1,0x1F49B,0x1F49A,0x1F499,0xF49C,0x1F584];
-
+//Click heart emoji to change colors "I ❤️ hot dogs"
 function display_randomHeart(){
   const heartArray = ["🧡 ","💛 ", "💚 ","💙 ","💜 ","🖤 "];
   const randomIndex = Math.floor(heartArray.length * Math.random());
@@ -28,19 +18,3 @@ function display_randomHeart(){
 
   document.getElementById("hearts").innerText = randomValue;
 }
-
-
-
-
-
-
-
-//var myArray = [
-  //"Apples",
-//  "Bananas",
-//  "Pears"
-//];
-
-//var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-
-//document.body.innerHTML = randomItem;
